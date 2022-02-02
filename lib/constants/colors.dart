e868b9c08167a713ca8e097d6aa5bc7eb7c89786
zsiecr <piyushnagpal777@gmail.com>
2022-02-02 21:03:37 +0530

    Basic Setup

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color tmLightYellow = Color(0xFFFFF7EC);
+const Color tmYellow = Color(0xFFFAF0DA);
+const Color tmDarkYellow = Color(0xFFEBBB7F);
+const Color tmLightRed = Color(0xFFFCF0F0);
+const Color tmRed = Color(0xFFFBE4E6);
+const Color tmDarkRed = Color(0xFFF08A8E);
+const Color tmLightBlue = Color(0xFFEDF4FE);
+const Color tmBlue = Color(0xFFE1EDFC);
+const Color tmDarkBlue = Color(0xFFC0D3F8);
